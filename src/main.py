fdfa9af2f8911433bdb6cfb1a75c7015b5958a31
root <root@localhost>
2025-07-05 20:25:08 +0300

    auto publish 1751736307391

--- src/main.py
+++ src/main.py
@@ -1,58 +1,48 @@
-import pygame
+from tkinter import *
+import tkinter.font as font
+def add():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x+y
 
-screen_size = [360, 600]
-screen = pygame.display.set_mode(screen_size)
 
-background = pygame.image.load('background.png')
-spaceship = pygame.image.load('spaceship.png')
-bullet = pygame.image.load('bullet.png')
-bullet_y = 500
-fired = False
+def sub():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x-y
 
-planets = ['p_one.png', 'p_two.png', 'p_three.png']
-p_index = 0
-planet = pygame.image.load(planets[p_index])
-planet_x = 140
-move_direction = 'right'
 
-keep_alive = True
-clock = pygame.time.Clock()
+def div():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x/y
 
-while keep_alive:
-   pygame.event.get()
-   keys = pygame.key.get_pressed()
-   if keys[pygame.K_SPACE] == True:
-       fired = True
 
-   if fired is True:
-       bullet_y = bullet_y - 5
-       if bullet_y == 50:
-           fired = False
-           bullet_y = 500
+def mul():
+    x = int(enterOne.get())
+    y = int(enterTwo.get())
+    result["text"] = x*y
+root = Tk()
+numOne = Label(root, text="Enter First Number:", font=("Serif", 12))
+enterOne = Entry(root)
+numTwo = Label(root, text="Enter Second Number:", font=("Serif", 12))
+enterTwo = Entry(root)
 
-   screen.blit(background, [0, 0])
-   screen.blit(bullet, [180, bullet_y])
-   screen.blit(spaceship, [160, 500])
+add = Button(root, text="Add", width=10, command=add)
+sub = Button(root, text="Sub", width=10, command=sub)
+div = Button(root, text="Div", width=10, command=div)
+mul = Button(root, text="Mul", width=10, command=mul)
 
-   if move_direction == 'right':
-       planet_x = planet_x + 5
-       if planet_x == 300:
-           move_direction = 'left'
-   else:
-       planet_x = planet_x - 5
-       if planet_x == 0:
-           move_direction = 'right'
+result = Label(root)
 
-   screen.blit(planet, [planet_x, 50])
-  
-   if bullet_y < 80 and planet_x > 120 and planet_x < 180:
-       p_index = p_index + 1
-       if p_index < len(planets):
-           planet = pygame.image.load(planets[p_index])
-           planet_x = 10
-       else:
-           print('YOU WIN')
-           keep_alive = False
+numOne.grid(row=0, column=0)
+enterOne.grid(row=0, column=1)
+numTwo.grid(row=1, column=0)
+enterTwo.grid(row=1, column=1)
+add.grid(row=2, column=0)
+sub.grid(row=2, column=1)
+div.grid(row=3, column=0)
+mul.grid(row=3, column=1)
+result.grid(row=4, column=0)
 
-   pygame.display.update()
-   clock.tick(60)  
+root.mainloop()
